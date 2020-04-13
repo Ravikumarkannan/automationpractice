1bcbd34972739d3812f35d8b4db6094bd8642b82
ELCOT <ELCOT@Lenovo>
2020-04-13 07:36:58 -0700

    commit

--- OrangeHRM/src/main/java/com/atmecs/orangehrm/pages/WebTablePage.java
+++ OrangeHRM/src/main/java/com/atmecs/orangehrm/pages/WebTablePage.java
@@ -2,9 +2,9 @@ package com.atmecs.orangehrm.pages;
 
 import java.util.List;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
 import com.atmecs.orangehrm.helper.CommonHelper;
 import com.atmecs.orangehrm.helper.Waits;
@@ -37,9 +37,36 @@ public class WebTablePage {
 		}
 	}
 	
-	public static void deleteJobTitle(WebDriver driver,String checkBox,String delete) {
+	public static void validateTable(WebDriver driver,String title,String body) {
+		List <WebElement> list = CommonHelper.findElements(driver, title);
+		List <WebElement> list_body = CommonHelper.findElements(driver, body);
+		
+		Assert.assertEquals(list.get(1).getText(), "Job Title");
+		Assert.assertEquals(list.get(2).getText(), "Job Description");
+		
+		Assert.assertEquals(list_body.get(0).getText(), "AssistantManager");
+		Assert.assertEquals(list_body.get(1).getText(), "CEO");
+		Assert.assertEquals(list_body.get(2).getText(), "Finance Manager");
+		Assert.assertEquals(list_body.get(3).getText(), "HR Executive");
+		Assert.assertEquals(list_body.get(4).getText(), "HR Manager");
+		Assert.assertEquals(list_body.get(5).getText(), "IT Executive");
+		Assert.assertEquals(list_body.get(6).getText(), "Manger");
+		Assert.assertEquals(list_body.get(6).getText(), "Mgr");
+		Assert.assertEquals(list_body.get(6).getText(), "Sales Executivems");
+		
+	}
+	
+	public static void deleteJobTitle(WebDriver driver,String checkBox,String delete,String deleteBox) {
 		CommonHelper.clickElement(driver, checkBox);
 		CommonHelper.clickElement(driver, delete);
-		driver.switchTo().alert().accept();
+		Waits.clickable(driver, deleteBox);
+		CommonHelper.clickElement(driver, deleteBox);
+	}
+	
+	public static void addJobTitle(WebDriver driver,String addBtn,String jobTitle,String title,String saveBtn) {
+		CommonHelper.clickElement(driver, addBtn);
+		Waits.explicitWait(driver, jobTitle);
+		CommonHelper.sendKeys(driver, jobTitle, title);
+		CommonHelper.clickElement(driver, saveBtn);
 	}
 }
--- OrangeHRM/src/test/java/com/atmecs/orangehrm/tests/WebTableValidation.java
+++ OrangeHRM/src/test/java/com/atmecs/orangehrm/tests/WebTableValidation.java
@@ -2,16 +2,18 @@ package com.atmecs.orangehrm.tests;
 
 import java.util.Properties;
 
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.atmecs.orangehrm.constants.FilePaths;
+import com.atmecs.orangehrm.helper.TestngListener;
 import com.atmecs.orangehrm.pages.WebTablePage;
 import com.atmecs.orangehrm.testbase.TestBase;
 import com.atmecs.orangehrm.utils.PropertyReader;
 
+@Listeners(TestngListener.class)
 public class WebTableValidation extends TestBase {
-
 	static Properties path;
 
 	@Test
@@ -26,7 +28,15 @@ public class WebTableValidation extends TestBase {
 		
 		WebTablePage.getTable(driver, path.getProperty("loc_webtable"),path.getProperty("l0c_webtable_body"));
 		
-		WebTablePage.deleteJobTitle(driver, path.getProperty("loc_checkbox"), path.getProperty("loc_delete_btn"));
+		WebTablePage.validateTable(driver, path.getProperty("loc_webtable"),path.getProperty("l0c_webtable_body"));
+		
+		WebTablePage.deleteJobTitle(driver, path.getProperty("loc_checkbox"), path.getProperty("loc_delete_btn"),path.getProperty("loc_delete_box"));
+
+		WebTablePage.addJobTitle(driver, path.getProperty("loc_add_btn"), path.getProperty("loc_jobtitle"), "AssistantManager",path.getProperty("loc_save_btn"));
+		
+		WebTablePage.addJobTitle(driver, path.getProperty("loc_add_btn"), path.getProperty("loc_jobtitle"), "DeputyManager",path.getProperty("loc_save_btn"));
+		
+		
 	}
 
 }
