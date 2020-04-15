fc8e172f2ae9d33972558377c92e3bc6763409e1
ELCOT <ELCOT@Lenovo>
2020-04-15 08:08:13 -0700

    commit

---
+++ SauceDemo/src/main/java/com/atmecs/saucedemo/constants/FilePath.java
@@ -0,0 +1,14 @@
+package com.atmecs.saucedemo.constants;
+
+public class FilePath {
+
+	/********Configuration Properties**********/
+	public static final String CHROME_DRIVER = "./driver/chromedriver.exe";
+	public static final String CONFIG_FILE = "./src/test/resources/config.properties";
+	
+	/********Locator Properties**********/
+	public static final String LOCATOR_HOMEPAGE = "./src/test/resources/locators/homepage.properties";
+	
+	/********Validation Properties**********/
+	public static final String VALID_HOMEPAGE = "./src/test/resources/validationdata/homepage.csv";
+}
